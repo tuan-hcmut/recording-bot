604772e24378abca6cad3fa3a627b55de677a2ad
tuan <tuan01677239677@gmail.com>
2024-03-17 12:57:32 +0700

    feat: recording

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,66 @@ RUN pip install --no-cache-dir --upgrade pip
 # Install Chrome and ChromeDriver dependencies
 RUN apt-get update && apt-get install -y wget gnupg curl unzip
 
+# Install some necessary dependencies
+RUN apt-get update && \
+    apt-get install -y \
+    pulseaudio \
+    pavucontrol \
+    sudo \
+    pulseaudio \
+    xvfb \
+    libnss3-tools \
+    ffmpeg \
+    xdotool \
+    x11vnc \
+    libfontconfig \
+    libfreetype6 \
+    xfonts-cyrillic \
+    xfonts-scalable \
+    fonts-liberation \
+    fonts-ipafont-gothic \
+    fonts-wqy-zenhei \
+    xterm \
+    vim
+
+RUN usermod -aG audio root
+RUN adduser root pulse-access
+
+ENV key=value DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
+ENV XDG_RUNTIME_DIR=/run/user/0
+
+
+RUN mkdir -p /run/dbus
+RUN chmod 755 /run/dbus
+
+RUN rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+
+
+RUN dbus-daemon --system --fork
+
+
+
+RUN wget http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz
+
+RUN tar -xvf pa_stable_v190700_20210406.tgz
+
+RUN mv portaudio /usr/src/
+
+WORKDIR /usr/src/portaudio
+
+RUN ./configure && \
+    make && \
+    make install && \
+    ldconfig
+
+RUN echo 'user ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+RUN adduser root pulse-access
+
+RUN mkdir -p /var/run/dbus
+
+RUN dbus-uuidgen > /var/lib/dbus/machine-id
+
 # Install Chrome
 RUN curl -sSL https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.198-1_amd64.deb -o chrome.deb
 RUN dpkg -i chrome.deb || apt-get install -fy
@@ -26,6 +86,12 @@ RUN rm chrome.deb /tmp/chromedriver.zip
 
 # Set working directory
 WORKDIR /usr/src/app
+
+RUN touch /root/.Xauthority
+RUN chmod 600 /root/.Xauthority
+
+RUN rm /run/dbus/pid
+RUN mv pulseaudio.conf /etc/dbus-1/system.d/pulseaudio.conf
 
 # Copy requirements.txt to the working directory
 COPY requirements.txt .
---
+++ screen-recording.py
@@ -0,0 +1,113 @@
+import asyncio
+import os
+import subprocess
+import datetime
+import requests
+import json
+
+from time import sleep
+
+import undetected_chromedriver as uc
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+async def run_command_async(command):
+    process = await asyncio.create_subprocess_shell(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    # Wait for the process to complete
+    stdout, stderr = await process.communicate()
+
+    return stdout, stderr
+
+async def join_meet():
+    print("starting virtual audio drivers")
+    # find audio source for specified browser
+    subprocess.check_output(
+        "sudo rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse", shell=True
+    )
+    subprocess.check_output(
+        "sudo pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit  >> /dev/null 2>&1",
+        shell=True,
+    )
+    subprocess.check_output(
+        'sudo pactl load-module module-null-sink sink_name=DummyOutput sink_properties=device.description="Virtual_Dummy_Output"',
+        shell=True,
+    )
+
+    subprocess.check_output(
+        'sudo pactl load-module module-null-sink sink_name=MicOutput sink_properties=device.description="Virtual_Microphone_Output"',
+        shell=True,
+    )
+    subprocess.check_output(
+        "sudo pactl set-default-source MicOutput.monitor", shell=True
+    )
+    subprocess.check_output("sudo pactl set-default-sink MicOutput", shell=True)
+    subprocess.check_output(
+        "sudo pactl load-module module-virtual-source source_name=VirtualMic",
+        shell=True,
+    )
+
+    options = uc.ChromeOptions()
+
+    options.add_argument("--use-fake-ui-for-media-stream")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-application-cache")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    log_path = "chromedriver.log"
+
+    driver = uc.Chrome(service_log_path=log_path, use_subprocess=False, options=options)
+
+    driver.set_window_size(1920, 1080)
+
+
+    # delete the folder screenshots if it exists even if not empty
+    print("Cleaning screenshots")
+    if os.path.exists("screenshots"):
+        # for each file in the folder delete it
+        for f in os.listdir("screenshots"):
+            os.remove(f"screenshots/{f}")
+    else:
+        os.mkdir("screenshots")
+    driver.execute_cdp_cmd(
+            "Browser.grantPermissions",
+            {
+                "origin": "https://google.com",
+                "permissions": [
+                    "geolocation",
+                    "audioCapture",
+                    "displayCapture",
+                    "videoCapture",
+                    "videoCapturePanTiltZoom",
+                ],
+            },
+        )
+
+
+    driver.get(f'https://google.com')
+    sleep(5)
+    driver.save_screenshot("screenshots/initial1.png")
+
+    duration = os.getenv("DURATION_IN_MINUTES", 1)
+    duration = int(duration) * 60
+
+    print("Start recording")
+    record_command = f"ffmpeg -y -video_size 1920x1080 -framerate 30 -f x11grab -i :99 -f pulse -i default -t {duration} -c:v libx264 -pix_fmt yuv420p -c:a aac -strict experimental recordings/output.mp4"
+
+    await asyncio.gather(
+        run_command_async(record_command),
+    )
+
+    print("Done recording")
+
+
+if __name__ == "__main__":
+    asyncio.run(join_meet())
