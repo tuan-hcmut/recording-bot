8702e031bbc397cbfd8314b44835e7c887dd895a
tuan <tuan01677239677@gmail.com>
2024-03-17 19:57:24 +0700

    feat: recording

--- screen-recording.py
+++ screen-recording.py
@@ -122,7 +122,7 @@ async def join_meet():
     driver.find_element(By.ID, 'input-for-pwd').send_keys("617108")
     driver.find_element(By.ID, 'input-for-name').send_keys("Qlay-bot")
     driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div[1]/div/div[2]/button').click()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
     driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div[2]/div/div[1]/div[1]/footer/div[1]/div[1]/div[1]/button').click()
     driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div[2]/div/div[1]/div[1]/div[8]/div[2]/div/div[2]/div/button').click()
     driver.save_screenshot("screenshots/c.png")
